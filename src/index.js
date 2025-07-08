1934dd1eec1db4035c543e320d18c835f8b02fc7
João Paulo Gonçalves Santos <github.broadcast509@passinbox.com>
2025-07-08 16:12:09 -0300

    Initial commit
    Project scaffolding and initial setup files added.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // O CSS que criamos antes
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);

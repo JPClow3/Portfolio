5be078fa3dc6ef25f1a20f1ecbd17e6c7d72540c
João Paulo Gonçalves Santos <joaopaulo.grv4@gmail.com>
2025-09-26 13:22:15 -0300

    Refactor tailwind.config.js for clarity and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-darkMode: 'class',
-content: [
-"./src/**/*.{js,jsx,ts,tsx,html}",
-"./public/index.html"
-],
-theme: {
-extend: {},
-},
-plugins: [
-// add Tailwind plugins here, for example:
-// require('@tailwindcss/forms'),
-],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };

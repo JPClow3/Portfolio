7963cdc161224bb7f85948eb3bbb3562029ec03f
João Paulo Gonçalves Santos <joaopaulo.grv4@gmail.com>
2025-09-26 13:16:54 -0300

    Add dark mode support to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    // add any Tailwind plugins here, e.g. require('@tailwindcss/forms')
-  ],
+darkMode: 'class',
+content: [
+"./src/**/*.{js,jsx,ts,tsx,html}",
+"./public/index.html"
+],
+theme: {
+extend: {},
+},
+plugins: [
+// add Tailwind plugins here, for example:
+// require('@tailwindcss/forms'),
+],
 };

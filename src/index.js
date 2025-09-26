a429b291f0a0bfdb6a699f24eba9d4f09530c7d1
João Paulo Gonçalves Santos <joaopaulo.grv4@gmail.com>
2025-09-26 14:04:53 -0300

    Wrap App component with AppProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // O CSS que criamos antes
 import App from './App';
+import { AppProvider } from './context/AppContext'; // 1. Importe o AppProvider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        {/* 2. Envolva o App com o AppProvider */}
+        <AppProvider>
+            <App/>
+        </AppProvider>
     </React.StrictMode>
 );
